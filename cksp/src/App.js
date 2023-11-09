df8be314ebccd312a9f0f3fc568bb98e5a0512c5
PedroRuama <ruama246@gmail.com>
2023-11-09 11:11:55 -0300

    add cksp and teste

---
+++ cksp/src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo_b.svg";
+import './App.css';
+
+function App(){
+  return (
+    <div className='back'>
+      <img src={logo} className="logo" alt="logo" />
+      
+      <p>selecione os ingredientes em sua despensa</p>
+      <div className='box'>
+        
+        <p id='subT'></p>
+      </div>
+      
+    </div>
+  );
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
