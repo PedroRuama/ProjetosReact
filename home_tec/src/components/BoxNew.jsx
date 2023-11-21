4fb62c3df40d6b1a16dff9ce2b34d432a3ec3284
PedroRuama <ruama246@gmail.com>
2023-11-21 14:47:54 -0300

    ajustes

--- home_tec/src/components/BoxNew.jsx
+++ home_tec/src/components/BoxNew.jsx
@@ -8,17 +8,17 @@ function BoxNew() {
             </div>
             
             <h3>Hydrogen V Eletric Cars</h3>
-            <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
+            <p className='p_boxnew'>Will hydrogen-fueled cars ever catch up to Evs?</p>
            
             <hr />
         
             <h3>The Downsides of AO Artistry</h3>
-            <p>What are the possible adverse affects of on-demand AI image generation?</p>
+            <p className='p_boxnew'>What are the possible adverse affects of on-demand AI image generation?</p>
           
             <hr />
         
             <h3>Is Vc Fundinding Drying Up</h3>
-            <p>Privete fundinding by VC firms is down 50% YOY. We take a look at that means.</p>
+            <p className='p_boxnew'>Privete fundinding by VC firms is down 50% YOY. We take a look at that means.</p>
 
            
 
