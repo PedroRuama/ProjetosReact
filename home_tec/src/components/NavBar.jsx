3dfb2a3576fb9aad89911814f673c6dc1e6d3f2e
PedroRuama <ruama246@gmail.com>
2023-11-26 23:46:58 -0300

    add: programação do menu lateral mobile

--- home_tec/src/components/NavBar.jsx
+++ home_tec/src/components/NavBar.jsx
@@ -2,19 +2,28 @@ import './nav.css'
 import logo from './imgs/logo.svg'
 import menu from './imgs/icon-menu.svg'
 import Xmenu from './imgs/icon-menu-close.svg'
+
+
+
 function NavBar() {
     return(
+        
         <div id='nav'>
             <img src={logo} alt="logo" id='logo'/>
-            <div id="nav-itens">            
-                <img src={menu} alt="menu" id='menu'/>
+
+            
+            <div id='sombra'></div>
+            <div id="nav-itens">           
+
+                {/* mobile menu  */} 
+                <img src={menu} alt="menu" id='menu' onClick={mobMenu}/>
                 <div id='anteLateral'>
                     <div id='lateral_div'>
                         <div id='Xmenu_div'>
-                            <img src={Xmenu} alt="menu" id='Xmenu'/>
+                            <img src={Xmenu} alt="Xmenu" id='Xmenu' onClick={mobMenu}/>
                         </div>
                         <br />
-                        <ul>
+                        <ul id='mob_itens'>
                             <li><a href="#">Home</a></li>
                             <li><a href="#">New</a></li>
                             <li><a href="#">Popular</a></li>
@@ -33,8 +42,35 @@ function NavBar() {
                 </ul>
             </div>
         </div>
-    );
-    
+        );
+        
 }
 
 export default NavBar;
+var i = 1
+function mobMenu() {
+    var sombra = document.getElementById("sombra")
+    var lateral = document.getElementById("lateral_div")
+    var menu = document.getElementById("menu")
+    var X = document.getElementById("Xmenu")
+    i = i * (-1)
+    
+    
+    console.log(i);
+    if (i < 0) {
+        menu.style = "display= none";
+        sombra.style = "display: block;"
+        lateral.style = "display: block;" 
+        lateral.style = "width: 60vw"
+      
+    } else{
+        menu.style = "display= flex";
+        sombra.style = "display: none;"
+        lateral.style = "display: none;" 
+        lateral.style = "width: 0vw"
+    }
+    
+       
+}
+
+
